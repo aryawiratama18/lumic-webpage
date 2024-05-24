9ad05a16a64682b4df8b7524223827d7296a555b
Nauval Aryawiratama <aryawiratamanauval@gmail.com>
2024-05-25 02:44:27 +0700

    Fix show more use case button

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,15 +1,37 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const ButtonComponent = ({ href = null, onClick= null, children, className = '', type = 'button' }) => {
+const ButtonComponent = ({
+  href = null,
+  onClick = null,
+  children,
+  className = "",
+  type = "button",
+}) => {
   const commonClasses = `py-2 px-3 w-full md:w-auto text-center font-semibold rounded-md ${className}`;
+
+  const handleClick = (e) => {
+    if (href && !onClick) {
+      e.preventDefault();
+      const targetId = href.slice(1); // Remove the '#' from the href
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+    if (onClick) {
+      onClick(e);
+    }
+  };
 
   if (href) {
     return (
       <a
         href={href}
         className={commonClasses}
-        style={{ cursor: 'pointer' }} 
-        onClick={onClick}
+        style={{ cursor: "pointer" }}
+        onClick={handleClick}
       >
         {children}
       </a>
@@ -21,7 +43,7 @@ const ButtonComponent = ({ href = null, onClick= null, children, className = '',
       type={type}
       onClick={onClick}
       className={commonClasses}
-      style={{ cursor: 'pointer' }} 
+      style={{ cursor: "pointer" }}
     >
       {children}
     </button>
--- src/section/ShowcaseSection.jsx
+++ src/section/ShowcaseSection.jsx
@@ -1,16 +1,24 @@
 import CardComponent from "../components/CardComponent";
 import ButtonComponent from "../components/ButtonComponent";
 import { caseStudy } from "../constants";
+import { NavLink } from "react-router-dom";
 
 const ShowcaseSection = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center rounded-md bg-gradient-to-r from-primary-light to-primary-dark p-5" data-aos="flip-up" data-aos-duration="1500">
+      <div
+        className="flex flex-col items-center justify-center rounded-md bg-gradient-to-r from-primary-light to-primary-dark p-5"
+        data-aos="flip-up"
+        data-aos-duration="1500"
+      >
         <div className="text-2xl md:text-3xl font-semibold mb-5 max-w-screen-md text-center text-white">
           ESL Usage in Grocery Market
         </div>
         <div className="w-full max-w-screen-md">
-          <div className="relative overflow-hidden" style={{ paddingTop: '56.25%' }}>
+          <div
+            className="relative overflow-hidden"
+            style={{ paddingTop: "56.25%" }}
+          >
             <iframe
               className="absolute top-0 left-0 w-full h-full"
               src="https://www.youtube.com/embed/lXBT7eVbBrk?si=BxTwS-_eN-BSYYI3"
@@ -22,7 +30,11 @@ const ShowcaseSection = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-full md:w-auto md:flex-row justify-center items-center md:items-stretch mt-6 space-y-4 md:space-y-0 md:space-x-4" data-aos="fade-up" data-aos-duration="1500">
+      <div
+        className="flex flex-col w-full md:w-auto md:flex-row justify-center items-center md:items-stretch mt-6 space-y-4 md:space-y-0 md:space-x-4"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
         {caseStudy.map((item, index) => (
           <CardComponent
             key={index}
@@ -32,11 +44,16 @@ const ShowcaseSection = () => {
           />
         ))}
       </div>
-      <div className="flex mt-4 py-4 w-full px-4 md:px-0 md:w-auto justify-center" data-aos="fade-up" data-aos-duration="1700">
-        <ButtonComponent 
-        href="/use-case"
-        className="bg-gradient-to-r from-primary-light to-primary-dark text-white"
-        >Show More Case</ButtonComponent>
+      <div
+        className="flex mt-4 py-4 w-full px-4 md:px-0 md:w-auto justify-center"
+        data-aos="fade-up"
+        data-aos-duration="1700"
+      >
+        <NavLink to="/use-case">
+          <ButtonComponent className="bg-gradient-to-r from-primary-light to-primary-dark text-white">
+            Show More Case
+          </ButtonComponent>
+        </NavLink>
       </div>
     </div>
   );
