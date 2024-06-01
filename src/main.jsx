25f97d54f441ff63e61b9deecd574f8a98ef5293
Nauval Aryawiratama <aryawiratamanauval@gmail.com>
2024-06-01 10:11:55 +0700

    Add speed insights component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
 import { Analytics } from "@vercel/analytics/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTopComponent from "./components/ScrollToTopComponent.jsx";
 
@@ -15,8 +15,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Analytics>
-        <ScrollToTopComponent />
-        <App />
+        <SpeedInsights>
+          <ScrollToTopComponent />
+          <App />
+        </SpeedInsights>
       </Analytics>
     </BrowserRouter>
   </React.StrictMode>
